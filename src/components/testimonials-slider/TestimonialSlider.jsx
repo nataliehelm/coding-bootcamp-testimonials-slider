ea63c295f71782405abe8ee4f7ca16127e0762c1
Natalia Garcia <nahiara@gmail.com>
2023-01-30 00:08:29 -0300

    feat: adding responsive styles

--- src/components/testimonials-slider/TestimonialSlider.jsx
+++ src/components/testimonials-slider/TestimonialSlider.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from "./TestimonialSlider.module.scss";
 
 function TestimonialSlider({ ...data }) {
   const [index, setIndex] = useState(0);
   const [prev, setPrev] = useState(false);
-  const [next, setNext] = useState(false);
+  const [next, setNext] = useState(true);
 
   const handleOnClickPrev = () => {
     if (index === 0) {
@@ -14,7 +14,6 @@ function TestimonialSlider({ ...data }) {
       setIndex(index - 1);
     }
   };
-
   const handleOnClickNext = () => {
     if (index >= Object.keys(data).length - 1) {
       setNext(false);
@@ -23,31 +22,44 @@ function TestimonialSlider({ ...data }) {
       setIndex(index + 1);
     }
   };
+
   return (
-    <div className="container">
-      <div className="left-container">
-        <img src={`images/${data[index].image}`}></img>
-        <div className="controls">
+    <div className={styles.box}>
+      <div className={styles.container}>
+        <div className={styles["left-container"]}>
           <img
-            src="images/icon-prev.svg"
-            onClick={handleOnClickPrev}
-            className={`arrow-${prev}`}
+            src={`images/${data[index].image}`}
+            className={styles["slider-image"]}
           ></img>
-          <img
-            src="images/icon-next.svg"
-            onClick={handleOnClickNext}
-            className={next ? "active-arrow" : "inactive-arrow"}
-          ></img>
+          <div className={styles["controls"]}>
+            <img
+              src="images/icon-prev.svg"
+              onClick={handleOnClickPrev}
+              className={
+                prev ? styles["active-arrow"] : styles["inactive-arrow"]
+              }
+            ></img>
+            <img
+              src="images/icon-next.svg"
+              onClick={handleOnClickNext}
+              className={
+                next ? styles["active-arrow"] : styles["inactive-arrow"]
+              }
+            ></img>
+          </div>
+        </div>
+        <div className={styles["right-container"]}>
+          <div className={styles.testimonial}>
+            <p>{data[index].testimonial}</p>
+          </div>
+          <div className={styles["personal-info"]}>
+            <p className={styles.name}>{data[index].name}</p>
+            <p className={styles.position}>{data[index].position}</p>
+          </div>
         </div>
       </div>
-      <div className="right-container">
-        <div className="testimonial">
-          <p>{data[index].testimonial}</p>
-        </div>
-        <div className="personal-info">
-          <p>{data[index].name}</p>
-          <p>{data[index].position}</p>
-        </div>
+      <div className={styles["pattern-curve"]}>
+        <img src="images/pattern-curve.svg"></img>
       </div>
     </div>
   );
